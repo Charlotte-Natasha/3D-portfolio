5fdf554488bd429564d6548782771bed3e935ab6
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-21 00:14:18 +0200

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ const Container = styled.div `
   scroll-behavior: smooth;
   overflow-y: auto;
   scrollbar-width: none;
+  color: white;
+  background: url("./img/b-g.jpg");
   &::-webkit-scrollbar{
     display: none;
   }
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 const Section = styled.div`
     height: 100vh;
-    background-color: red;
+    scroll-snap-align: center;
 `;
 
 const Works = () => {
