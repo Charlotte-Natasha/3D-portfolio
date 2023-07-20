a6032b2a2df556f683331d8e62f01ace04787fdd
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-20 21:05:27 +0200

    added all the req components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import Hero from './components/Hero'
+import Pay from './components/Pay'
+import Who from './components/Who'
+import Works from './components/Works'
 
 function App() {
 
   return (
     <>
-      
-        
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Pay/>
     </>
   )
 }
