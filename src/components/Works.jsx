721f6a4ebe90a13fc82a3f20faa977463c7add82
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-21 15:10:13 +0200

    added all components needed in works comp

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,12 +3,39 @@ import { styled } from "styled-components";
 const Section = styled.div`
     height: 100vh;
     scroll-snap-align: center;
+    display: flex;
+    justify-content: center;
+`;
+
+const Container= styled.div`
+    width: 1400;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+`;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
+
+const Right = styled.div`
+    flex: 1;
 `;
 
 const Works = () => {
     return (
     <Section>
-        Work
+        <Container>
+            <Left>
+                <List>
+                    <ListItem></ListItem>
+                </List>
+            </Left>
+            <Right></Right>
+        </Container>
     </Section>
     );
 };
