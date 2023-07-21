7bccd67d4e1575777357ccb1a5c24562a413e7d2
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-21 14:41:22 +0200

    added animation to the image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,6 +75,13 @@ const Img = styled.img`
     left: 0;
     right: 0;
     margin: auto;
+    animation: animate 2s infinite ease alternate;
+
+    @keyframes animate {
+        to {
+            transform: translateY(25px)
+        }
+    }
 `;
 
 const Hero = () => {
