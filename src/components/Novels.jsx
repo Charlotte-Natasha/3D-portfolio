7b6aef3d2cd87443f5654b72e9121dd6f2c3a256
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-25 15:22:02 +0200

    added books and flowers jsx and gltf components

--- src/components/Novels.jsx
+++ src/components/Novels.jsx
@@ -1,13 +1,13 @@
 import { OrbitControls, Stage } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-import Series from './Series'
+import Books from './Books'
 
 
 const Novels = () => {
   return (
     <Canvas>
       <Stage environment="city" intensity={0.6}>
-        <Series/>
+        <Books/>
         <OrbitControls enableZoom={false}/>
       </Stage>
     </Canvas>
