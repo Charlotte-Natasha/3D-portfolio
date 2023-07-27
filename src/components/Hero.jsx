389084944c5471d62be9d5056d49439e63d3994c
Charlotte Natasha Onyango <natashaonyango97@gmail.com>
2023-07-27 23:12:40 +0200

    made the hero component responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,14 +11,25 @@ const Section = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
+
+    @media only screen and (max-width: 780px) {
+        height: 200vh;
+    }
 `;
 
 const Container = styled.div`
-    height: 100vh;
+    height: 100%;
     scroll-snap-align: center;
     width: 1400px;
     display: flex;
     justify-content: space-between;
+
+    @media only screen and (max-width: 780px) {
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 `;
 
 const Left = styled.div`
@@ -27,9 +38,18 @@ const Left = styled.div`
     flex-direction: column;
     justify-content: center;
     gap: 20px;
+
+    @media only screen and (max-width: 780px) {
+        flex: 1;
+        align-items: center;
+    }
 `;
 const Title = styled.h1`
     font-size: 74px;
+
+    @media only screen and (max-width: 780px) {
+        text-align: center;
+    }
 `;
 
 const WhoIAm = styled.div`
@@ -50,6 +70,11 @@ const Subtitle = styled.h2`
 const Description = styled.p`
     font-size: 24px;
     color: lightgray;
+
+    @media only screen and (max-width: 780px) {
+        padding: 20px;
+        text-align: center;
+    }
 `;
 
 const Button = styled.button`
@@ -65,6 +90,11 @@ const Button = styled.button`
 const Right = styled.div`
     flex: 3;
     position: relative;
+
+    @media only screen and (max-width: 780px) {
+        flex: 1;
+        width: 100%;
+    }
 `;
 
 const Img = styled.img`
@@ -72,16 +102,21 @@ const Img = styled.img`
     height: 70vh;
     object-fit: contain;
     position: absolute;
-    top:0;
+    top: 0;
     bottom: 0;
     left: 0;
     right: 0;
     margin: auto;
     animation: animate 2s infinite ease alternate;
 
+    @media only screen and (max-width: 780px) {
+        width: 300px;
+        height: 300px;
+    }
+
     @keyframes animate {
         to {
-            transform: translateY(25px)
+        transform: translateY(25px);
         }
     }
 `;
